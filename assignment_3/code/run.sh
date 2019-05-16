029e631241652cd5370b8989f1f95352c74b8243
Maurice Frank <maurice.frank@posteo.de>
2019-05-16 16:19:47 +0200

    run script

--- assignment_3/code/run.sh
+++ assignment_3/code/run.sh
@@ -6,7 +6,7 @@ echo "#!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mem=60000M
 #SBATCH --partition=gpu_shared_course
 #SBATCH --gres=gpu:1
